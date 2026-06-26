4cd4e7f5cded03c4e9cbb3b217ae3e2a04ca148f
Dheive Rodrigues Fuck <Dheiverege@gmail.com>
2026-06-25 21:48:48 -0300

    feat:Aula 14

--- Projeto-jogo-memoria/assests/scripts/home.js
+++ Projeto-jogo-memoria/assests/scripts/home.js
@@ -1,38 +1,27 @@
+const btn = document.getElementById("reset")
+const card = [
+    "/Projeto-jogo-memoria/assests/images/51FX7BB8gbL._AC_UF894,1000_QL80_.jpg",
+    "/Projeto-jogo-memoria/assests/images/bandeira-da-alemanha.jpg",
+    "/Projeto-jogo-memoria/assests/images/bandeira-da-franca.jpg",
+    "/Projeto-jogo-memoria/assests/images/bandeiradobrasil-2-cke.webp",
+    "/Projeto-jogo-memoria/assests/images/Espanha-1111x740.jpg",
+    "/Projeto-jogo-memoria/assests/images/Flag_of_Argentina.svg.webp",
+    "/Projeto-jogo-memoria/assests/images/Flag_of_Uruguay.svg.webp",
+    "/Projeto-jogo-memoria/assests/images/Italia-1111x740.jpg"
+];
+let blocked = false;
+let parsFound = 0;
+let moves = 0;
 
-const blocked = false
-
-
-const array = [
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img"),
-document.createElement("img")
-]
-
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+function shuffle(card) {
+    for (let i = card.length - 1; i > 0; i--) {
         // Sorteia um índice de 0 a i
         const j = Math.floor(Math.random() * (i + 1));
 
         // Troca os elementos de lugar
-        [array[i], array[j]] = [array[j], array[i]];
+        [card[i], card[j]] = [card[j], card[i]];
     }
-    return array;
-
-
+    return card;
 }
 
 function starGame() {
@@ -47,5 +36,7 @@ function checkPair() {
 
 }
 
-
+btn.addEventListener("click", () => {
+    
+})
 
